66131ec30cc5d3124f98e276e5c68ebbf24f033e
DiKrrr3233 <82635209+DiKrrr3233@users.noreply.github.com>
2023-04-09 01:33:25 +0800

    Update fields_outputs.py

--- gprMax/fields_outputs.py
+++ gprMax/fields_outputs.py
@@ -26,7 +26,12 @@ from gprMax.grid import Ix, Iy, Iz
 
 def store_outputs(iteration, Ex, Ey, Ez, Hx, Hy, Hz, G):
     """Stores field component values for every receiver and transmission line.
-
+    
+    您选择的文本是一个名为 store_outputs 的函数，它用于存储每个接收器和传输线的场分量值。
+    该函数接受当前迭代次数、电场和磁场值以及一个包含模型描述的必要参数的网格类实例作为输入。
+    该函数遍历所有接收器并存储电场或磁场分量，如果输出中包含 ‘I’，则存储电流分量。
+    对于传输线，该函数存储总电压和总电流
+    
     Args:
         iteration (int): Current iteration number.
         Ex, Ey, Ez, Hx, Hy, Hz (memory view): Current electric and magnetic field values.
@@ -50,6 +55,10 @@ def store_outputs(iteration, Ex, Ey, Ez, Hx, Hy, Hz, G):
 
 
 kernel_template_store_outputs = Template("""
+
+kernel_template_store_outputs 的模板，它定义了一个 CUDA 内核，用于在 GPU 上执行 store_outputs 函数的功能。
+这个内核使用宏来将下标转换为线性索引，并定义了一个名为 store_outputs 的全局函数，
+该函数接受接收器总数、迭代次数、接收器坐标、接收器数组以及电场和磁场数组作为输入，并存储每个接收器的场分量值。
 
 // Macros for converting subscripts to linear index:
 #define INDEX2D_RXCOORDS(m, n) (m)*($NY_RXCOORDS)+(n)
@@ -92,7 +101,14 @@ __global__ void store_outputs(int NRX, int iteration, const int* __restrict__ rx
 
 def write_hdf5_outputfile(outputfile, G):
     """Write an output file in HDF5 format.
-
+    write_hdf5_outputfile 的函数，它用于将模拟结果写入 HDF5 格式的输出文件。
+    
+    该函数接受输出文件的名称和一个包含模型描述的必要参数的网格类实例作为输入。
+    
+    该函数首先创建一个 HDF5 文件并添加一些属性，如 gprMax 版本、模型标题、迭代次数、网格大小、空间分辨率、时间步长、源数量、接收器数量、源步长和接收器步长等。
+    然后，该函数为除传输线外的所有源创建组，并添加类型和位置数据属性。对于传输线，该函数创建组并添加位置数据、线电阻和线离散化属性，并写入线电压和电流数组。
+    最后，该函数为接收器创建组，添加位置数据并写入场分量数组。
+    
     Args:
         outputfile (str): Name of the output file.
         G (class): Grid class instance - holds essential parameters describing the model.
